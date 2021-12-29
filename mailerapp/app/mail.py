2076dc7aca338ff64a91b8275aa1a3752418e71d
Jose Carlos Colque Montaño <montanoj47@gmail.com>
2021-12-29 19:58:52 -0400

    Primer Avance

--- mailerapp/app/mail.py
+++ mailerapp/app/mail.py
@@ -1,9 +1,13 @@
 from flask import (
-    Blueprint
+    Blueprint,render_template
 )
-
+from app.db import get_db
 bp = Blueprint('mail', __name__, url_prefix='/')
 
 @bp.route('/', methods=['GET'])
 def index():
-    return 'mail'
+    db, c = get_db()
+    c.execute('SELECT * FROM email')
+    mails = c.fetchall()
+    print(mails)
+    return render_template('mails/index.html', mails=mails)
