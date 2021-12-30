7d764b7ce718040277dc1f7a6c080b7d1d5e14f5
Jose Carlos Colque Montaño <montanoj47@gmail.com>
2021-12-30 11:23:13 -0400

    form, email, validations, css

--- mailerapp/app/mail.py
+++ mailerapp/app/mail.py
@@ -1,6 +1,11 @@
 from flask import (
-    Blueprint,render_template
+    Blueprint,render_template,request,flash,redirect,url_for,session,g,current_app
 )
+import sendgrid 
+from sendgrid.helpers.mail import *
+
+from flask.helpers import url_for
+from werkzeug.utils import redirect
 from app.db import get_db
 bp = Blueprint('mail', __name__, url_prefix='/')
 
@@ -11,3 +16,38 @@ def index():
     mails = c.fetchall()
     print(mails)
     return render_template('mails/index.html', mails=mails)
+
+@bp.route('/create', methods=['GET', 'POST'])
+def create():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        subject = request.form.get('subject')
+        content = request.form.get('content')
+        errors = []
+        if not email:
+            errors.append('Email is required')
+        if not subject:
+            errors.append('subject is required')
+        if not content:
+            errors.append('content is required')
+        if len(errors) == 0:
+            db, c = get_db()
+            # send(email, subject, content)
+            c.execute('INSERT INTO email (email, subject, content) VALUES (%s, %s, %s)', (email, subject, content))
+            db.commit()
+            return redirect(url_for('mail.index'))
+            
+        else:
+            for error in errors:
+                flash(error)
+    return render_template('mails/create.html')
+
+def send(to, subject, content):
+    sg = sendgrid.SendGridAPIClient(api_key=current_app.config['SENDGRID_KEY'])
+    from_email = Email(current_app.config['FROM_EMAIL'])
+    to_email = To(to)
+    content = Content('text/plain',content)
+    mail = Mail(from_email,to_email,subject,content)
+    response = sg.client.mail.send.post(request_body=mail.get())
+    print(response)
+    
